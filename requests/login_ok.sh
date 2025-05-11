59fb2e2fd7ffe2529cd59b7f3ce131aadac90265
FireBlast <fireblast901@gmail.com>
2025-05-11 23:18:46 +0000

    adicionar requests das rotas todo

--- requests/login_ok.sh
+++ requests/login_ok.sh
@@ -1,11 +1,3 @@
-: ${APP_HOST:="http://localhost:8080"}
 echo login ok
-curl --request POST -s \
-  --url $APP_HOST/login \
-  --header 'Content-Type: application/json' \
-  --header 'User-Agent: insomnia/11.0.2' \
-  --data '{
-	"email": "test@example.com",
-	"password": "helloworld"
-}'
+bash ./requests/login_ok_req.sh
 echo
--- requests/registro_ok.sh
+++ requests/registro_ok.sh
@@ -1,11 +1,4 @@
 : ${APP_HOST:="http://localhost:8080"}
 echo registro ok
-curl --request POST \
-  --url $APP_HOST/register \
-  --header 'Content-Type: application/json' \
-  --data '{
-	"username": "test",
-	"email": "test@example.com",
-	"password": "helloworld"
-}'
+bash requests/registro_ok_req.sh
 echo
